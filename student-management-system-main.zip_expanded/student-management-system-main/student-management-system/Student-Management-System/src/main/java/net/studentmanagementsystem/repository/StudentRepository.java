9b6e2085d2dba0fb1a466213f24ed9e9ac6e2212
MohammedAksheikh <eng91mhmd@gmail.com>
2025-06-18 00:55:41 +0300

    testing

--- student-management-system-main.zip_expanded/student-management-system-main/student-management-system/Student-Management-System/src/main/java/net/studentmanagementsystem/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package net.studentmanagementsystem.repository;
-
-import net.studentmanagementsystem.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-
-}
